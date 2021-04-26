22f24011ec97aa6470ea2c8f844157c0b3095994
J-Phiz <jp.save@gmail.com>
2021-04-26 11:55:04 +0200

    import from spring initializr

---
+++ jpsave/src/test/java/org/jps/jpsave/JpsaveApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jps.jpsave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpsaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
