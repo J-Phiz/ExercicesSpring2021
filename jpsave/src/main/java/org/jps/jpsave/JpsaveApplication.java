1e1734ab0eb8f712527f6ca800aeb7526cf6f9e5
J-Phiz <jp.save@gmail.com>
2021-04-26 12:33:40 +0200

    Use 4 spaces indent

--- jpsave/src/main/java/org/jps/jpsave/JpsaveApplication.java
+++ jpsave/src/main/java/org/jps/jpsave/JpsaveApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JpsaveApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JpsaveApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JpsaveApplication.class, args);
+    }
 
 }
--- jpsave/src/test/java/org/jps/jpsave/JpsaveApplicationTests.java
+++ jpsave/src/test/java/org/jps/jpsave/JpsaveApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class JpsaveApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
