e6a84604152bf692a32d69112010cc2b7f9ca94f
J-Phiz <jp.save@gmail.com>
2021-04-27 11:22:02 +0200

    Add Integration Tests

--- jpsave/src/test/java/org/jps/jpsave/JpsaveApplicationTests.java
+++ jpsave/src/test/java/org/jps/jpsave/JpsaveApplicationTests.java
@@ -1,13 +1,50 @@
 package org.jps.jpsave;
 
+import org.jps.jpsave.service.OrderService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class JpsaveApplicationTests {
 
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private OrderService orderService;
+
+
     @Test
-    void contextLoads() {
+    void getOrder() throws Exception {
+        orderService.addOrder("bob");
+
+        mvc.perform(get("/order/0").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.name", is("bob")));
+    }
+
+    @Test
+    void getOrders() throws Exception {
+        orderService.addOrder("bob");
+        orderService.addOrder("bob2");
+
+        mvc.perform(get("/order/list").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].id", is(0)))
+                .andExpect(jsonPath("$[0].name", is("bob")))
+                .andExpect(jsonPath("$[1].id", is(1)))
+                .andExpect(jsonPath("$[1].name", is("bob2")));
     }
 
 }
